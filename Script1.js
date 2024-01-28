e1fdb551be07b6d100b017dc1730e698af7def03
Patryk-Pyrek <148455592+PatrykPyrek@users.noreply.github.com>
2024-01-28 21:52:29 +0100

    28.01.2024

--- Script1.js
+++ Script1.js
@@ -1,1 +1,13 @@
-// JavaScript source code
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var timeString = addZeroBefore(hours) + ':' + addZeroBefore(minutes) + ':' + addZeroBefore(seconds);
+    document.getElementById('Zegaros').textContent = timeString;
+}
+function addZeroBefore(number) {
+    return number < 10 ? '0' + number : number;
+}
+setInterval(updateClock, 1000);
+updateClock();
